68cda240a07a15ee6d12973295343962a9aa9533
Feiran Zhang <feiranzhang@outlook.com>
2020-08-25 21:08:47 +0800

    Update inspectorscripts.ts

--- src/inspectorscripts.ts
+++ src/inspectorscripts.ts
@@ -254,7 +254,7 @@ def _jupyterlab_variableinspector_deletevariable(x):
     has_no_dim <- is.na(obj.dim)[1:length(names)]                        
     obj.dim[has_no_dim, 1] <- napply(names, length)[has_no_dim]
     
-    vec <- (obj.type != "function" & obj.type != 'Seurat' & obj.type != 'CellDataSet' & obj.size < 1048576)
+    vec <- (obj.type != "function" & obj.type != 'Seurat' & obj.type != 'CellDataSet' & obj.type != 'AnnotatedDataFrame' & obj.size < 1048576)
     obj.content[vec] <- napply(names[vec], function(x) toString(x, width = 154)[1])
                       
     # obj.rownames <- napply(names, rownames)
