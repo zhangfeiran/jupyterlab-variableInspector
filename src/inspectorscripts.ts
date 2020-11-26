cc88ab72edff17ecc88cdc0b54f7bae9c93988c5
Feiran Zhang <feiranzhang@outlook.com>
2020-11-26 13:12:33 +0800

    add .Last.value

--- src/inspectorscripts.ts
+++ src/inspectorscripts.ts
@@ -243,6 +243,8 @@ def _jupyterlab_variableinspector_deletevariable(x):
     napply <- function(names, fn) sapply(names, function(x) fn(get(x, 
         pos = pos)))
     names <- ls(pos = pos, pattern = pattern)
+    names = c(names,'.Last.value')
+
     if (length(names) == 0) {
         return(jsonlite::toJSON(data.frame()))
     }
